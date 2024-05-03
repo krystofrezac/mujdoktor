8379ef459b86a52dbdf5879a08398fcb772efdd8
Kryštof Řezáč <krystofrezac@gmail.com>
2024-05-03 14:17:38 +0200

    Improve home page

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 export const HomePage = () => {
 	return (
-		<div className="d-flex justify-content-center">
-			<Link to="/reservation/procedure">
-				<Button>Create reservation</Button>
+		<div className="d-flex flex-column align-items-center">
+			<h1>Doctor Arnostřep Šílený</h1>
+			<h2>Consectetur adipiscing elit. Donec vitae dui nec orci!</h2>
+			<Link className="mt-2" to="/reservation/procedure">
+				<Button size="lg">Create reservation</Button>
 			</Link>
 		</div>
 	);
